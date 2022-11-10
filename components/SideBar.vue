7b4c29ae8751704e6443a5061bd8e77862b276b6
AungPaingSoe <aungpaingsoe097238@gmail.com>
2022-11-10 16:40:08 +0630

    bootstrap icon i

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -2,7 +2,7 @@
   <div>
     <h5 class="mb-0 py-2">Nuxt Frame</h5>
     <ul class=" w-100 list-unstyled ">
-      <li class="rounded-5 text-center text-black">
+      <li class="rounded-5 text-start text-black">
         <NuxtLink class="nav-link" to="dashboard">Dashboard</NuxtLink>
       </li>
     </ul>
